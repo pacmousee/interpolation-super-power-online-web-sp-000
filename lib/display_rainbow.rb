bb897feab8f1e97464a59b87a8c5164c23a5cd26
impartial-router-9741 <elena.tjahjono@yahoo.com>
2021-01-07 10:43:18 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -8,5 +8,6 @@ I= indigo,
 V= violet]
 
 def display_rainbow (colors)
-  puts "R, O, Y, G, B, I, V"
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
+"
