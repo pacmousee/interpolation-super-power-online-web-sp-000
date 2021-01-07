4e906d384f55f89c6df893f64ddba0f3baf90859
impartial-router-9741 <elena.tjahjono@yahoo.com>
2021-01-07 10:27:15 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,6 @@
 # Write your #display_rainbow method here
-def display_rainbow
-  puts ["R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"]
+colors = ["R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"]
+
+def display_rainbow (colors)
+  puts ["R", "O", "Y", "G", "B", "I", "V"]
 end
