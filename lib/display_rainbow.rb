28e71e06a3052540c3cb50f7e49bd47d4ec80ef5
impartial-router-9741 <elena.tjahjono@yahoo.com>
2021-01-07 10:30:38 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,5 +2,5 @@
 colors = ["R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"]
 
 def display_rainbow (colors)
-  puts "R", "O", "Y", "G", "B", "I", "V"
+  puts "#{colors["0","1","2","3","4","5","6"]}"
 end
